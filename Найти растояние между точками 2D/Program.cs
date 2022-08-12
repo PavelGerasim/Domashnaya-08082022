4b95194b60531a598d1037726a63e004fd37ab31
Pavel <Pavel_Gerasimov_98@mail.ru>
2022-08-12 12:49:44 +0400

    2D расстояние

---
+++ Найти растояние между точками 2D/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите первое натуральное число: ");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе натуральное число: ");
+int y = int.Parse(Console.ReadLine());
+int length = 0;
+
+if(x < y)
+{
+    length = y - x;
+}
+else
+{
+    if(x > y)
+    {
+        length = x - y;
+    }
+}
+Console.WriteLine("Расстояние равно: ");
+Console.WriteLine(length);
