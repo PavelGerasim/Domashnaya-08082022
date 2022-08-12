4df13dbbdf34ee4fd214d7d5ee18e9eb83948c8c
Pavel <Pavel_Gerasimov_98@mail.ru>
2022-08-12 11:11:01 +0400

    Произведение чисел

---
+++ Произведение чисел от 1 до х/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите крайнее число: ");
+int x = int.Parse(Console.ReadLine());
+int count = 1;
+int final = 1;
+
+while(count <= x)
+{
+    final = final * count;
+    count++;
+}
+Console.WriteLine(final);
