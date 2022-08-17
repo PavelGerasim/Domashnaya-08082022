145009dafe07cf5dfa90bbb2267923a5d8499053
Pavel <Pavel_Gerasimov_98@mail.ru>
2022-08-17 08:01:49 +0400

    Расстояние между 2мя точками

--- Найти растояние между точками 2D/Program.cs
+++ Найти растояние между точками 2D/Program.cs
@@ -1,19 +1,11 @@
-﻿Console.WriteLine("Введите первое натуральное число: ");
-int x = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите второе натуральное число: ");
-int y = int.Parse(Console.ReadLine());
-int length = 0;
-
-if(x < y)
-{
-    length = y - x;
-}
-else
-{
-    if(x > y)
-    {
-        length = x - y;
-    }
-}
-Console.WriteLine("Расстояние равно: ");
-Console.WriteLine(length);
+﻿Console.Write("Введите x1: ");
+double x1 = double.Parse(Console.ReadLine());
+Console.Write("Введите y1: ");
+double y1 = double.Parse(Console.ReadLine());
+Console.Write("Введите x2: ");
+double x2 = double.Parse(Console.ReadLine());
+Console.Write("Введите y2: ");
+double y2 = double.Parse(Console.ReadLine());
+double result = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+Console.WriteLine($"Расстояние между точками: {result}");
+Console.ReadLine();
